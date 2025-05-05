b62e53a7a0a8ca403fd85684820f276e65d50836
zhuoqingli <143445503+zhuoqingli526@users.noreply.github.com>
2025-05-05 23:38:50 +0000

    adds director page

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -3,4 +3,12 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/list"})
   end
 
+  def show
+    the_id = params.fetch("the_id")
+    matching_records = Director.where({ :id => the_id})
+    @the_director = matching_records.at(0)
+    render({ :template => "director_templates/details"})
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
   get("/directors",  { :controller => "directors", :action => "index" })
+
+
+  get("/directors/:the_id",  { :controller => "directors", :action => "show" })
 end
