64728ed0f561f96b766d4871fe96cc75b866f70a
zhuoqingli <143445503+zhuoqingli526@users.noreply.github.com>
2025-05-06 00:06:41 +0000

    Update movie and director views with new detail layout

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,7 +6,7 @@ class MoviesController < ApplicationController
   def show
     the_id = params.fetch("the_id")
     matching_records = Movie.where({ :id => the_id})
-    @the_director = matching_records.at(0)
+    @the_movie = matching_records.at(0)
     render({ :template => "movie_templates/details"})
 
   end
